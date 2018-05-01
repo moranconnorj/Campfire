33bfb01a52cf0a958eae7be66f5d8e0b7164435b
Connor Moran <moranconnorj@gmail.com>
2018-05-01 15:29:31 -0700

    added signup to parse

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -12,7 +12,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -31,6 +33,33 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+  public void signUpClicked(View view) {
+      EditText usernameEditText = findViewById(R.id.usernameEditText);
+      EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+      if (usernameEditText.getText().toString() == "" || passwordEditText.getText().toString() == "") {
+          Toast.makeText(this, "A username and password are required", Toast.LENGTH_SHORT).show();
+      } else {
+          ParseUser user = new ParseUser();
+          user.setUsername(usernameEditText.getText().toString());
+          user.setPassword(passwordEditText.getText().toString());
+
+          user.signUpInBackground(new SignUpCallback() {
+              @Override
+              public void done(ParseException e) {
+                  if (e == null) {
+                      // OK
+                      Log.i("Sign Up", "OK");
+                  } else {
+                      Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                      e.printStackTrace();
+                  }
+              }
+          });
+      }
+  }
+
 
 
   @Override
