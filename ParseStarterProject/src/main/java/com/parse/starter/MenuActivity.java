dd60fe40654da0571f6b1da7ecbca4fbaf971cb3
Connor Moran <moranconnorj@gmail.com>
2018-05-15 14:14:59 -0700

    Added beginning of groupExists function and hideKeyboard functionality to MenuActivity

--- ParseStarterProject/src/main/java/com/parse/starter/MenuActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MenuActivity.java
@@ -1,14 +1,17 @@
 package com.parse.starter;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -17,8 +20,13 @@ import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
     EditText addGroupEditText;
+
+    public void onClick(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    }
 
     public void logout(View view) {
         ParseUser.logOut();
@@ -44,11 +52,18 @@ public class MenuActivity extends AppCompatActivity {
         });
     }
 
+    public boolean groupExists() {
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         addGroupEditText = findViewById(R.id.addGroupEditText);
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+
+        constraintLayout.setOnClickListener(this);
 
 
         ListView listView = findViewById(R.id.listView);
@@ -60,19 +75,20 @@ public class MenuActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    if (addGroupEditText.getText().length() > 0) {
-                        createGroup();
-                    } else {
-//                        Groupname length < 0
+                if (position == 0) { // Create Group Clicked
+                    if (addGroupEditText.getText().length() > 0) { // Group Name Not Blank
+                        if (groupExists()) { // Group/User combo already exists in Parse
+                            Toast.makeText(MenuActivity.this, "Group Already Exists", Toast.LENGTH_SHORT).show();
+                        } else { // Group/User combo DOES NOT exist in parse; create group
+                            createGroup();
+                        }
+                    } else { //Groupname length < 0
+                        Toast.makeText(MenuActivity.this, "Group Name Required", Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     Log.i("group selected", groupnames.get(position));
                 }
             }
         });
-
-
-
     }
 }
